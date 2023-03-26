2b6f26265a5d0efeecb558e09a83ca38f4b8df48
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-03-27 00:41:51 +0100

    shorter solution

--- largestOfFour.js
+++ largestOfFour.js
@@ -10,4 +10,7 @@ function largestOfFour(arr) {
   
   let count = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
   console.log(count)
+
+
+  //return arr.map((item)=> Math.max(...item));
   
