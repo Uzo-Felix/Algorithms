fb6190a2d2cbed5c4f187151c04e73011d26e459
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-04-07 09:28:55 +0100

    fear not letters

---
+++ missing_Letters.js
@@ -0,0 +1,14 @@
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+function fearNotLetter(str) {
+    for(let i= 0; i< str.length-1; i++){
+      if(str.charCodeAt(i+1) - str.charCodeAt(i) != 1){
+        return String.fromCodePoint(str.charCodeAt(i) + 1);
+      }
+    } return undefined
+    }
+    
+    let result = fearNotLetter("abce");
+    console.log(result);
